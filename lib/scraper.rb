36fc9c7c28e194cfef641a477e981726d12eee64
Rachel Wyatt <rw126@evansville.edu>
2018-10-07 21:55:05 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,8 @@ class Scraper
 
     def get_page
       html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+      Nokogiri::HTML(html)
+      doc = Nokogiri:HTML(html)
     end
 
     def print_courses
